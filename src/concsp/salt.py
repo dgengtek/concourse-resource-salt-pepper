de72ecb073bd5ee9403f66550dd9c5781db5b668
dgengtek <dgengtek@users.noreply.github.com>
2018-07-24 23:44:07 +0200

    Additional logging for pepper run

--- src/concsp/salt.py
+++ src/concsp/salt.py
@@ -28,9 +28,12 @@ class SaltAPI():
         cache for returns for the job.
         '''
         load['client'] = 'local_async'
+        logger.info("Running local_async low")
         async_ret = self.pepper.low(load)
         jid = async_ret['return'][0]['jid']
+        logger.info("jid: {}".format(jid))
         nodes = async_ret['return'][0]['minions']
+        logger.info("nodes: {}".format(nodes))
         ret_nodes = []
         exit_code = 1
 
@@ -39,11 +42,13 @@ class SaltAPI():
         start_time = time.time()
         exit_code = 0
         while True:
+            logger.info("waiting for all expected nodes to return")
             total_time = time.time() - start_time
             if total_time > self.payload.timeout:
                 exit_code = 1
                 break
 
+            logger.info("Runner jobs.lookup_jid for jid: {}".format(jid))
             jid_ret = self.pepper.low([{
                 'client': 'runner',
                 'fun': 'jobs.lookup_jid',
@@ -53,6 +58,7 @@ class SaltAPI():
             }])
 
             responded = set(jid_ret['return'][0].keys()) ^ set(ret_nodes)
+            logger.info("Nodes responded: {}".format(responded))
             for node in responded:
                 yield None, "{{{}: {}}}".format(
                     node,
@@ -60,9 +66,11 @@ class SaltAPI():
             ret_nodes = list(jid_ret['return'][0].keys())
 
             if set(ret_nodes) == set(nodes):
+                logger.info("All nodes responded. Finishing.")
                 exit_code = 0
                 break
             else:
+                logger.info("Not all nodes responded. Trying again...")
                 time.sleep(self.payload.seconds_to_wait)
 
         if self.payload.fail_if_minions_dont_respond:
@@ -74,7 +82,21 @@ class SaltAPI():
             list(set(ret_nodes) ^ set(nodes)))
 
     def run(self, load):
-        self.login()
+        try:
+            self.login()
+        except PepperException as exc:
+            logger.error(
+                    "{}. Failed to login as {} using eauth: {}."
+                    .format(exc, self.payload.username, self.payload.eauth))
+
+        logger.info("Running pepper - {} {} - '{}' {} {} {} ".format(
+            load.client,
+            load.expr_form,
+            load.tgt,
+            load.fun,
+            load.args,
+            load.kwargs))
+
         for exit_code, result in self.poll_for_returns(load):
             print(exit_code, result)
 
