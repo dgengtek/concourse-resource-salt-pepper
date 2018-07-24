616405b80d267d0ce8000be70e6ef8f2690ed62f
dgengtek <dgengtek@users.noreply.github.com>
2018-07-24 17:55:18 +0200

    Implement concourse api

--- src/concsp/api.py
+++ src/concsp/api.py
@@ -1,17 +1,77 @@
+from concsp import salt, payload
 import abc
 
+
 class ConcourseApi(abc.ABC):
-    def __init__(self):
+    def __init__(self, payloadapi, saltpepperapi):
+        self.payloadapi = payloadapi
+        self.saltpepperapi = saltpepperapi
+
+    def _execute(self):
+        """
+        Run resource specific api
+        """
+        pepper = self.saltpepperapi(self.payload)
+        pepper_payload = salt.get_api_payload(self.payload)
+        pepper.run(pepper_payload)
+
+    def run(self):
+        self._input()
+        self._execute()
+        self._output()
+
+    def _input(self):
+        self.payload = self.payloadapi()
+        self.payload.init()
+
+    @abc.abstractmethod
+    def _output(self):
+        raise NotImplementedError()
+
+
+class ConcourseApiNoop(ConcourseApi):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def run(self):
+        pass
+
+    def _output(self):
         pass
 
 
-class ConcourseCheck(ConcourseApi):
-    pass
+class ConcourseApiIn(ConcourseApiNoop):
+    def _output(self):
+        import json
+        print(json.dumps({
+            "version": {},
+            }))
 
 
-class ConcourseIn(ConcourseApi):
-    pass
+class ConcourseApiCheck(ConcourseApi):
+    def _output(self):
+        print([])
 
 
-class ConcourseOut(ConcourseApi):
-    pass
+class ConcourseApiOut(ConcourseApi):
+    def _output(self):
+        import json
+        print(json.dumps({
+            "version": {},
+            }))
+
+
+def build_check():
+    return ConcourseApiCheck(
+            payload.ConcoursePayload,
+            salt.SaltAPI)
+
+
+def build_out():
+    return ConcourseApiOut(
+            payload.ConcoursePayloadOut,
+            salt.SaltAPI)
+
+
+def build_in():
+    return ConcourseApiIn()
