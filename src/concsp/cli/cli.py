e3486c80658ab8f87d98e11b295ebdc9e7d1dd05
dgengtek <dgengtek@users.noreply.github.com>
2018-07-25 17:25:42 +0200

    Try without click

--- src/concsp/cli/cli.py
+++ src/concsp/cli/cli.py
@@ -1,47 +1,19 @@
-import click
-# http://click.pocoo.org/5/options/
 from concsp import api
-import logging
+import sys
 
 
-@click.group("concsp")
-@click.option(
-    "-l",
-    "--loglevel",
-    type=click.Choice(["info", "debug", "warning"]),
-    default="info")
-@click.pass_context
-def main(ctx, loglevel):
-    levels = {
-        "info": logging.INFO,
-        "debug": logging.DEBUG,
-        "warning": logging.WARNING,
-        }
-    logging.basicConfig(level=levels.get(loglevel))
-    d = dict()
-    ctx.obj = d
-
-
-@main.command("check")
-@click.pass_context
-def main_check(ctx, args=None):
-    concourse = api.build_check()
-    concourse.run()
-
-
-@main.command("in")
-@click.argument("destination", required=True)
-@click.pass_context
-def main_in(ctx, destination):
-    concourse = api.build_in()
-    concourse.run()
-
-
-@main.command("out")
-@click.argument("source", required=True)
-@click.pass_context
-def main_out(ctx, source):
-    concourse = api.build_out()
+def main():
+    try:
+        cmd = sys.argv[1]
+    except IndexError:
+        return
+    concourse = None
+    if cmd == "in":
+        concourse = api.build_in()
+    elif cmd == "out":
+        concourse = api.build_out()
+    elif cmd == "check":
+        concourse = api.build_check()
     concourse.run()
 
 
